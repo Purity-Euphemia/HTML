0b25d7e13a0cd84a95aad61110dbf63e666a9b1c
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-29 03:49:36 +0100

    Add files via upload

---
+++ doom/index.js
@@ -0,0 +1,24 @@
+const wrapper = document.getElementById("wrapper")
+
+//console.log(wrapper);
+
+const titles = document.getElementsByClassName("title")
+//console.log(titles)
+
+const list = document.getElementsByTagName("li")
+//console.log(list)
+//console.log(Array.isArray(list));
+
+const liArray = Array.from(list)
+//console.log(liArray);
+
+//console.log(Array.isArray(liArray))
+
+//const ul = document.querySelector("ul")
+//console.log(ul)
+
+const ul = document.querySelector("#book-list")
+console.log(ul)
+
+const ull = document.querySelectorAll("li")
+//console.log(ull)
